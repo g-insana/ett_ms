2cf1e0e822319b3af918a29d4e6656fd74f0b54d
Giuseppe Insana <insana@ebi.ac.uk>
2026-05-18 10:03:42 +0100

    path fix

--- analysis_code/down_fasta_dir_oscode_acc.sh
+++ analysis_code/down_fasta_dir_oscode_acc.sh
@@ -7,6 +7,4 @@ shift
 acc=$*
 ## echo $OSCODE $acc
 
-/hps/software/users/martin/uniprot/src/anaconda3/envs/python385/bin/extractor.py -f/hps/nobackup/martin/uniprot/production/temp/mmseqs/$DIR/$OSCODE.FASTA -i /hps/nobackup/martin/uniprot/production/temp/mmseqs/$DIR/$OSCODE.FASTA.idx -s $acc 2> down_fasta.err
-
-## /hps/software/users/martin/uniprot/src/anaconda3/envs/python385/bin/extractor.py -f/hps/nobackup/martin/uniprot/production/temp/mmseqs/fasta/$OSCODE.FASTA -i /hps/nobackup/martin/uniprot/production/temp/mmseqs/fasta/$OSCODE.FASTA.idx -s $1 2> $prefix.err
+extractor.py -f $DIR/$OSCODE.FASTA -i $DIR/$OSCODE.FASTA.idx -s $acc 2> down_fasta.err
--- analysis_code/down_fasta_dir_oscode_acc2.sh
+++ analysis_code/down_fasta_dir_oscode_acc2.sh
@@ -7,5 +7,5 @@ shift
 acc=$*
 ## echo $OSCODE $acc
 
-/hps/software/users/martin/uniprot/src/anaconda3/envs/python385/bin/extractor.py -f/hps/nobackup/martin/uniprot/production/temp/mmseqs/$DIR/$OSCODE.FASTA -i /hps/nobackup/martin/uniprot/production/temp/mmseqs/$DIR/$OSCODE.FASTA.idx2 -s $acc 2> down_fasta.err
+extractor.py -f $DIR/$OSCODE.FASTA -i $DIR/$OSCODE.FASTA.idx2 -s $acc 2> down_fasta.err
 
